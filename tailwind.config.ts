65e8bf96bfbd1bfd45262912f94aee445ef534ee
Alex Szpakiewicz <szpakiewicz.alex@gmail.com>
2024-01-17 10:28:57 +0100

    feat: NextUi only works on client side so i reverted

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'tailwindcss'
-const {nextui} = require("@nextui-org/react");
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     colors: {
       transparent: 'transparent',
@@ -24,7 +24,6 @@ const config: Config = {
       darkBlock: '#2E2E3D',
     }
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
 export default config
