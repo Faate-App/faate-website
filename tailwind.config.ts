5c4ce41aaf3a07dfd9288863d7adf693612e3eee
Alex Szpakiewicz <szpakiewicz.alex@gmail.com>
2024-01-17 09:36:30 +0100

    feat: copy of NUXT website onto Next, with tailwind colors missing buttons and whats new

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,23 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      background: '#f9f9f9', // Light background to keep it fresh and clean
+      accent: '#FF6B6B', // Soft red for a romantic touch
+      accentHover: '#C15054', // Soft red for a romantic touch
+      headline: '#2C2C2C', // Dark color for text to ensure readability
+      paragraph: '#4B4B4B', // Slightly lighter text color for contrast
+      secondary: '#FFC75F', // Warm secondary color to make CTAs stand out
+      tertiary: '#FF61A6', // A vibrant pink to add a playful, romantic vibe
+      darkBackground: '#1e1e2b',  // Darker background
+      darkHeadline: '#E0E0E0',  // Light color for text on dark background
+      darkParagraph: '#B0B0B0',  // Lighter text color for contrast
+      darkBlock: '#2E2E3D',
+    }
   },
   plugins: [],
 }
